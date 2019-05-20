2510723f066cecf8b6d6f2b3db6cbf6c21b2f36f
Jinchen Hu <jinchenhu.luke@gmail.com>
2019-05-20 13:05:21 -0400

    Update Driver.java

--- Part2/Driver.java
+++ Part2/Driver.java
@@ -1,15 +1,4 @@
-//-------------------------------
-//Assignment #2
-//Question: Part - 2;
-//Written by: Jinchen Hu ID#40080398
-/**
-* 
-* <p>@author Jinchen Hu ID#40080398</p>
-* <p>COMP 249</p>
-* <p>Assignment #2</p>
-* <p>Due 11:59 PM - Wednesday, February 20, 2019</p>
-*
-*/
+
 package part21;
 
 import part21.AirCraft.ClassType;
